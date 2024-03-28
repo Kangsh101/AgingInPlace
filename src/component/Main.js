787a93db6c887c947f8f39868f03b8e8392a40b1
SukHyeon Kang <kangsh4969@naver.com>
2024-03-28 14:24:58 +0900

    ww TEST

--- src/component/Main.js
+++ src/component/Main.js
@@ -21,7 +21,7 @@ const Main = () => {
               onClick={() => handleSectionClick('whatIsDementia')}
               className={selectedSection === 'whatIsDementia' ? 'active' : ''}
             >
-              정민테스트용 글자바꿈?
+              정민이란?
             </button>
             <button
               onClick={() => handleSectionClick('causes')}
