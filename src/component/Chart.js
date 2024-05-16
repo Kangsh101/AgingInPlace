01b4f23dfc4d98645c2e3902cd12c29dcf272bdb
LeeSeok0 <deca5636@gmail.com>
2024-05-16 13:20:07 +0900

    chart.js 로직 수정

--- src/component/Chart.js
+++ src/component/Chart.js
@@ -5,6 +5,7 @@ import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Responsive
 const Chart = () => {
   const [data1, setData1] = useState([]);
   const [data2, setData2] = useState([]);
+  const [error, setError] = useState('');
 
   useEffect(() => {
     const fetchData = async () => {
@@ -12,20 +13,24 @@ const Chart = () => {
         const response1 = await axios.post('/chart/steps', { username: 'Lee' });
         const response2 = await axios.post('/chart/calories', { username: 'Lee' });
 
-        const formattedData1 = response1.data.map(item => ({
-          name: new Date(item['activity_data.create_date']).toLocaleDateString(),
-          value: item['activity_data.activity_steps_total']
-        }));
+        if (response1.data && Array.isArray(response1.data)) {
+          const formattedData1 = response1.data.map(item => ({
+            name: new Date(item['activity_data.create_date']).toLocaleDateString(),
+            value: item['activity_data.activity_steps_total']
+          }));
+          setData1(formattedData1);
+        }
 
-        const formattedData2 = response2.data.map(item => ({
-          name: new Date(item['activity_data.create_date']).toLocaleDateString(),
-          value: item['activity_data.activity_cal_total']
-        }));
-
-        setData1(formattedData1);
-        setData2(formattedData2);
+        if (response2.data && Array.isArray(response2.data)) {
+          const formattedData2 = response2.data.map(item => ({
+            name: new Date(item['activity_data.create_date']).toLocaleDateString(),
+            value: item['activity_data.activity_cal_total']
+          }));
+          setData2(formattedData2);
+        }
       } catch (error) {
-        console.error('Error fetching data', error);
+        console.error('데이터 가져오기 오류', error);
+        setError('데이터 가져오는 중 오류가 발생했습니다.');
       }
     };
     fetchData();
@@ -33,6 +38,7 @@ const Chart = () => {
 
   return (
     <div className='content-main'>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
       <ResponsiveContainer width="100%" height={400}>
         <BarChart data={Array.isArray(data1) ? data1 : []} margin={{ top: 20, right: 30, left: 20, bottom: 5 }}>
           <CartesianGrid strokeDasharray="3 3" />
