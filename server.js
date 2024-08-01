d83623a7673f236aab0411f44813395ecace3dc7
hshy1839 <hongjeongmin1839@gmail.com>
2024-08-01 23:47:25 +0900

    jenkins test push

--- server.js
+++ server.js
@@ -243,6 +243,44 @@ app.get('/api/android/userinfo', (req, res) => {
   );
 });
 
+//CIST question 가져오기 모바일
+app.get('/api/android/cist_questions', (req, res) => {
+  // 쿠키에서 사용자 ID를 추출합니다.
+  const userId = req.cookies.userId;
+
+  // 사용자 ID를 사용하여 데이터베이스에서 사용자 정보를 가져옵니다.
+  connection.query(
+    "SELECT type, question_text, answer_options, phoneNumber, birthdate, email FROM members WHERE id = ?;",
+    [userId], // userId 값을 플레이스홀더에 전달
+    (err, rows, fields) => {
+      if (err) {
+        console.error('회원 정보 조회 실패: ' + err.stack);
+        res.status(500).json({ error: '회원 정보 조회 실패' }); // JSON 형식으로 오류 응답
+        return;
+      }
+
+      if (rows.length === 0) {
+        console.error('사용자 정보가 없습니다.');
+        res.status(404).json({ error: '사용자 정보가 없습니다.' }); // JSON 형식으로 오류 응답
+        return;
+      }
+
+      // 조회된 사용자 정보를 JSON 형식으로 응답
+      const user = rows[0];
+      const userInfo = {
+        gender: user.gender,
+        name: user.name,
+        role: user.role,
+        phoneNumber: user.phoneNumber,
+        birthdate: user.birthdate,
+        email: user.email
+      };
+      res.status(200).json(userInfo); // JSON 형식으로 사용자 정보 응답
+    }
+  );
+});
+
+
 // 사용자 정보 업데이트
 app.post('/api/updateuserinfo', (req, res) => {
   const userId = req.session.userId;
